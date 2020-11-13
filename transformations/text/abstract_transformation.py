7d410e73b952233c006e9129b7142150e41ed735
fabriceyhc <fabriceyhc@gmail.com>
2020-11-13 02:06:32 -0800

    initial check-in
    Added abstract base class for transformations with example of emojify / demojify

---
+++ transformations/text/abstract_transformation.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+ 
+class AbstractTransformation(ABC):
+    """
+    An abstract class for transformations to be applied 
+    to input data. 
+    """
+
+    @abstractmethod
+    def __init__(self, **kwargs):
+        """
+        Initializes the transformation and provides an
+        opporunity to supply a configuration if needed
+        """
+        self.config = config
+    
+    @abstractmethod
+    def __call__(self, string):
+        """
+        Apply the transformation using config as support
+
+        Parameters
+        ----------
+        string : str
+            the input string
+        """
+        pass
