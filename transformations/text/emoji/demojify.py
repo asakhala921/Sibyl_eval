035265ab041ad6d626677682fdba4c75dc27d8ec
fabriceyhc <fabriceyhc@gmail.com>
2020-11-28 12:51:24 -0800

    adding targeted addition / removal of emoji based on sentiment

--- transformations/text/emoji/demojify.py
+++ transformations/text/emoji/demojify.py
@@ -1,5 +1,5 @@
 from ..abstract_transformation import AbstractTransformation, _get_tran_types
-from emoji_translate import Translator
+from emoji_translate import Translator, emoji_lis
 
 class Demojify(AbstractTransformation):
     def __init__(self, exact_match_only=False, randomize=True):
@@ -34,6 +34,99 @@ class Demojify(AbstractTransformation):
             The output with all emojis removed.
         """
         return self.emo.demojify(string)
+
+    def get_tran_types(self, task_name=None, tran_type=None):
+        self.tran_types = {
+            'task_name': ['sentiment', 'topic'],
+            'tran_type': ['INV', 'INV']
+        }
+        df = _get_tran_types(self.tran_types, task_name, tran_type)
+        return df
+
+class RemoveEmoji(Demojify):
+    def __init__(self, polarity=[-1, 1]):
+        """
+        Initializes the transformation and provides an
+        opporunity to supply a configuration if needed
+        
+        Parameters
+        ----------
+        polarity : list
+            Emoji sentiment is measured in polarity 
+            between -1 and +1. This param allows you
+            to pick the sentiment range you want.
+            - positivev ==> [0.05, 1]    
+            - negative ==> [-1, -0.05] 
+            - neutral ==> [-0.05, 0.05]
+        """
+        super().__init__(self) 
+        self.polarity = polarity
+
+    def __call__(self, string):
+        """
+        Parameters
+        ----------
+        string : str
+            The string input
+
+        Returns
+        ----------
+        ret : str
+            The output with emojis in the target polarity
+            range removed.
+        """
+        return self.remove_emoji_by_polarity(string, self.polarity)
+
+    def get_tran_types(self, task_name=None, tran_type=None):
+        pass
+
+    def remove_emoji_by_polarity(self, string, p_rng=[-1,1]):
+        for emoji_data in emoji_lis(string)[::-1]:
+            i, emoji = emoji_data.values()
+            _polarity = self.emo.get_df_by_emoji(emoji)['polarity'].iloc[0]
+            if p_rng[0] <= _polarity <= p_rng[1]:
+                string = string[:i] + '' + string[i + 1:].lstrip()
+        return string.rstrip()
+
+class RemovePositiveEmoji(RemoveEmoji):
+    def __init__(self, polarity=[0.05, 1]):
+        super().__init__(self) 
+        self.polarity = polarity
+
+    def __call__(self, string):
+        return self.remove_emoji_by_polarity(string, self.polarity)
+
+    def get_tran_types(self, task_name=None, tran_type=None):
+        self.tran_types = {
+            'task_name': ['sentiment', 'topic'],
+            'tran_type': ['SIB', 'INV']
+        }
+        df = _get_tran_types(self.tran_types, task_name, tran_type)
+        return df
+
+class RemoveNegativeEmoji(RemoveEmoji):
+    def __init__(self, polarity=[-1, -0.05]):
+        super().__init__(self) 
+        self.polarity = polarity
+
+    def __call__(self, string):
+        return self.remove_emoji_by_polarity(string, self.polarity)
+
+    def get_tran_types(self, task_name=None, tran_type=None):
+        self.tran_types = {
+            'task_name': ['sentiment', 'topic'],
+            'tran_type': ['SIB', 'INV']
+        }
+        df = _get_tran_types(self.tran_types, task_name, tran_type)
+        return df
+
+class RemoveNeutralEmoji(RemoveEmoji):
+    def __init__(self, polarity=[-0.05, 0.05]):
+        super().__init__(self) 
+        self.polarity = polarity
+
+    def __call__(self, string):
+        return self.remove_emoji_by_polarity(string, self.polarity)
 
     def get_tran_types(self, task_name=None, tran_type=None):
         self.tran_types = {
--- transformations/text/emoji/emojify.py
+++ transformations/text/emoji/emojify.py
@@ -43,3 +43,99 @@ class Emojify(AbstractTransformation):
         }
         df = _get_tran_types(self.tran_types, task_name, tran_type)
         return df
+
+class AddEmoji(Emojify):
+    def __init__(self, num=1, polarity=[-1, 1]):
+        """
+        Initializes the transformation and provides an
+        opporunity to supply a configuration if needed
+        
+        Parameters
+        ----------
+        num : int
+            The number of emojis to append to the end
+            of a given string
+        polarity : list
+            Emoji sentiment is measured in polarity 
+            between -1 and +1. This param allows you
+            to pick the sentiment range you want.
+            - positivev ==> [0.05, 1]    
+            - negative ==> [-1, -0.05] 
+            - neutral ==> [-0.05, 0.05]
+        """
+        super().__init__(self) 
+        self.num = num
+        self.polarity = polarity
+
+    def __call__(self, string):
+        """
+        Parameters
+        ----------
+        string : str
+            The string input
+
+        Returns
+        ----------
+        ret : str
+            The output with `num` emojis appended
+        """
+        return string + ' ' + ''.join(self.sample_emoji_by_polarity(self.polarity, self.num))
+
+    def get_tran_types(self, task_name=None, tran_type=None):
+        pass
+
+    def sample_emoji_by_polarity(self, p_rng, num=1):
+        emojis = self.emo.emojis
+        return emojis[emojis['polarity'].apply(
+            lambda x: p_rng[0] <= x <= p_rng[1])].sample(num)['char'].values.tolist()
+
+class AddPositiveEmoji(AddEmoji):
+    def __init__(self, num=1, polarity=[0.05, 1]):
+        super().__init__(self) 
+        self.num = num
+        self.polarity = polarity
+
+    def __call__(self, string):
+        return string + ' ' + ''.join(self.sample_emoji_by_polarity(self.polarity, self.num))
+
+    def get_tran_types(self, task_name=None, tran_type=None):
+        self.tran_types = {
+            'task_name': ['sentiment', 'topic'],
+            'tran_type': ['SIB', 'INV']
+        }
+        df = _get_tran_types(self.tran_types, task_name, tran_type)
+        return df
+
+class AddNegativeEmoji(AddEmoji):
+    def __init__(self, num=1, polarity=[-1, -0.05]):
+        super().__init__(self) 
+        self.num = num
+        self.polarity = polarity
+
+    def __call__(self, string):
+        return string + ' ' + ''.join(self.sample_emoji_by_polarity(self.polarity, self.num))
+
+    def get_tran_types(self, task_name=None, tran_type=None):
+        self.tran_types = {
+            'task_name': ['sentiment', 'topic'],
+            'tran_type': ['SIB', 'INV']
+        }
+        df = _get_tran_types(self.tran_types, task_name, tran_type)
+        return df
+
+class AddNeutralEmoji(AddEmoji):
+    def __init__(self, num=1, polarity=[-0.05, 0.05]):
+        super().__init__(self) 
+        self.num = num
+        self.polarity = polarity
+
+    def __call__(self, string):
+        return string + ' ' + ''.join(self.sample_emoji_by_polarity(self.polarity, self.num))
+
+    def get_tran_types(self, task_name=None, tran_type=None):
+        self.tran_types = {
+            'task_name': ['sentiment', 'topic'],
+            'tran_type': ['INV', 'INV']
+        }
+        df = _get_tran_types(self.tran_types, task_name, tran_type)
+        return df
